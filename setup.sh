66759450aaf2239a5dbccf09abeb0007b2a7e1b8
Barath raj <sbarathraj.ai@gmail.com>
2025-07-18 19:27:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Install Google Chrome
+# Exit on any error
+set -e
+
+# Update package index
 apt-get update
-apt-get install -y wget unzip curl
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Install required packages
+apt-get install -y wget curl gnupg unzip
+
+# Download and install Google Chrome
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 # Clean up
-rm google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
+apt-get clean
